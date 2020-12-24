728b5ee795d80a7378169487a1b6ff29ce3978c9
NyanLinHtut20318 <s10206260@connect.np.edu.sg>
2020-12-24 22:51:13 +0800

    ASG2

--- .js
+++ .js
@@ -1,4 +1,11 @@
 fetch("https://api.hearthstonejson.com/v1")
-  .then(res => res)
+  .then(res => {
+    if (res.ok) {
+      console.log('SUCCESS')
+    } else {
+      console.log('NOT SUCCESSFUL')
+    }
+  })
   .then(data => console.log(data))
+  .catch(error => console.log('ERROR'))
 
