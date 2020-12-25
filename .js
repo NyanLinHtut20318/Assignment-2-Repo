b21b09e3044449f6d65565c6891c8a84af890730
NyanLinHtut20318 <s10206260@connect.np.edu.sg>
2020-12-25 22:40:05 +0800

    ASG2

--- .js
+++ .js
@@ -1,11 +1,13 @@
-fetch("https://api.hearthstonejson.com/v1/68600/enUS/cards.json")
-  .then(res => {
-    if (res.ok) {
-      console.log('SUCCESS')
-    } else {
-      console.log('NOT SUCCESSFUL')
-    }
-  })
-  .then(data => console.log(data))
-  .catch(error => console.log('ERROR'))
-
+fetch("https://omgvamp-hearthstone-v1.p.rapidapi.com/cards", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "7f3858e219msh928ae11832ce33dp1e8daejsn8d6dafdb99b3",
+		"x-rapidapi-host": "omgvamp-hearthstone-v1.p.rapidapi.com"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
