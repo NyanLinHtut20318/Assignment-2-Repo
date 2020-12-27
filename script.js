18d6364c1761e642c88696fd947649bb477e46b4
NyanLinHtut20318 <s10206260@connect.np.edu.sg>
2020-12-28 02:14:17 +0800

    ASG2

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-fetch("https://us.api.blizzard.com/hearthstone/cards?locale=en_US&access_token=USeF28PwwtmKTHiUNGJjr8ONG99Vo32Bu3")
-.then(response => {
-	console.log(response);
+fetch("https://us.api.blizzard.com/hearthstone/cards?locale=en_US&access_token=USdBW2W6eVe6YXGuQCgv86WTx0eHg8n5Hk")
+.then((response) => {
+	return response.json();
 })
+.then((data) =>{
+	var Card = data.cards;
+	console.log(Card);
+}
+)
 .catch(err => {
 	console.error(err);
 });
