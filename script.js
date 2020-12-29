c5e8b8dc2befbfb21e8accbabaeaa392586bac57
NyanLinHtut20318 <s10206260@connect.np.edu.sg>
2020-12-30 05:28:01 +0800

    ASG2

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
-fetch("https://us.api.blizzard.com/hearthstone/cards?locale=en_US&access_token=USdBW2W6eVe6YXGuQCgv86WTx0eHg8n5Hk")
-.then((response) => {
-	return response.json();
-})
-.then((data) =>{
-	var Card = data.cards;
-	console.log(Card);
+
+function fetchData() {
+	fetch("https://us.api.blizzard.com/hearthstone/cards?locale=en_US&access_token=USRi5itcek9kug6foOvn9H7b0coICJLqgC")
+	.then((response) => {
+		console.log(response);
+		if (!response.ok) {
+			throw Error("ERROR");
+		}
+		return response.json();
+	})
+	.then((data) =>{
+		console.log(data);
+		const html = data.cards
+		  .map(user => {
+			  return `
+			  <div>
+			    <p><img src="${user.image}" alt="${user.name}" /></imga></p>
+			  </div>
+			  `;
+		  })
+		  .join("");;
+		document.querySelector("#allCards").innerHTML = html
+	})
+	.catch(err => {
+		console.error(err);
+	});
 }
-)
-.catch(err => {
-	console.error(err);
+const searchBar = document.getElementById("searchBar");
+let cardNames = [];
+console.log(searchBar);
+searchBar.addEventListener("keyup", (x) =>{
+	const searchString = x.target.value;
+	const filteredNames = cardNames.filter((name) => {
+		return (
+			cards.name.includes(searchString)
+		);
+	});
+	console.log(filteredNames);
 });
-	
+fetchData()
