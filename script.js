b40d672a8ebf26a0ca75a349b8a446270171c82a
NyanLinHtut20318 <s10206260@connect.np.edu.sg>
2020-12-31 18:47:31 +0800

    ASG2

--- script.js
+++ script.js
@@ -1,29 +1,7 @@
-const searchBar = document.getElementById("searchBar"); 
-console.log(searchBar);
-let searchNames = [
-	fetch("https://us.api.blizzard.com/hearthstone/cards?locale=en_US&access_token=USRi5itcek9kug6foOvn9H7b0coICJLqgC")
-	.then((response) => {
-		console.log(response);
-		if (!response.ok) {
-			throw Error("ERROR");
-		}
-		return response.json();
-	})
-	.catch(err => {
-		console.error(err);
-	})
-];
-searchBar.addEventListener("keyup", (x) =>{
-	const searchString = x.target.value;
-	const filteredNames = searchNames.filter((name) => {
-		return (
-			cards.name.includes(searchString)
-			);
-		});
-	console.log(filteredNames);
-    });
+const searchBox = document.querySelector(".searchBox");
+const inputBox = searchBox.querySelector("input");
 function fetchData() {
-	fetch("https://us.api.blizzard.com/hearthstone/cards?locale=en_US&access_token=USRi5itcek9kug6foOvn9H7b0coICJLqgC")
+	fetch("https://us.api.blizzard.com/hearthstone/cards?locale=en_US&access_token=USUrcY5UsNtLfJWQSRgM8mIu8zhTwmnXSl")
 	.then((response) => {
 		console.log(response);
 		if (!response.ok) {
@@ -51,3 +29,8 @@ function fetchData() {
 }
 
 fetchData()
+inputBox.onkeyup = (e)=>{
+	let userData = e.target.value;
+	console.log(userData);
+
+}
